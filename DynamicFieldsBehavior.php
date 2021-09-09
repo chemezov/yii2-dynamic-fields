2caee2e315ee007e229d116791d0c7e89c0bf75b
Mikhail Chemezov <michlenanosoft@gmail.com>
2021-09-10 01:47:05 +0500

    some refactor and optimization.

--- DynamicFieldsBehavior.php
+++ DynamicFieldsBehavior.php
@@ -11,6 +11,7 @@ use yii\base\Behavior;
 use yii\db\ActiveRecord;
 use yii\db\Query;
 use yii\helpers\ArrayHelper;
+use yii\helpers\StringHelper;
 
 class DynamicFieldsBehavior extends Behavior
 {
@@ -23,7 +24,8 @@ class DynamicFieldsBehavior extends Behavior
 
     /**
      * Fields to store and load with your model. Example: ['address', 'is_client'].
-     * @var array
+     *
+     * @var string[]
      */
     public $fields = [];
 
@@ -37,10 +39,10 @@ class DynamicFieldsBehavior extends Behavior
     public function events()
     {
         return [
-            ActiveRecord::EVENT_AFTER_FIND => 'afterFind',
-            ActiveRecord::EVENT_AFTER_INSERT => 'afterSave',
-            ActiveRecord::EVENT_AFTER_UPDATE => 'afterSave',
-            ActiveRecord::EVENT_BEFORE_DELETE => 'deleteModelValues',
+            ActiveRecord::EVENT_AFTER_FIND => 'loadDynamicFieldsValues',
+            ActiveRecord::EVENT_AFTER_INSERT => 'saveDynamicFieldsValues',
+            ActiveRecord::EVENT_AFTER_UPDATE => 'saveDynamicFieldsValues',
+            ActiveRecord::EVENT_BEFORE_DELETE => 'deleteDynamicFieldsValues',
         ];
     }
 
@@ -68,48 +70,52 @@ class DynamicFieldsBehavior extends Behavior
         }
     }
 
-    public function afterFind($event)
+    public function saveDynamicFieldsValues()
+    {
+        /* Save fields values in separate table */
+        $this->deleteDynamicFieldsValues();
+
+        if (!empty($this->fields)) {
+            $data = [];
+
+            foreach ($this->fields as $field) {
+                $data[] = [
+                    $this->getModelClass(),
+                    $this->getPrimaryKey(),
+                    $field,
+                    $this->owner->$field,
+                ];
+            }
+
+            Yii::$app->db->createCommand()->batchInsert($this->tableName, ['model', 'model_id', 'field', 'value'], $data)->execute();
+        }
+    }
+
+    public function loadDynamicFieldsValues()
     {
         /* Load fields values */
-        $query = (new Query())
-            ->select(['field', 'value'])
-            ->from($this->tableName)
-            ->where(['model' => $this->getModelClass(), 'model_id' => $this->getPrimaryKey()]);
+        if (!empty($this->fields)) {
+            $query = (new Query())
+                ->select(['field', 'value'])
+                ->from($this->tableName)
+                ->where(['model' => $this->getModelClass(), 'model_id' => $this->getPrimaryKey()]);
 
-        foreach (ArrayHelper::map($query->all(), 'field', 'value') as $field => $value) {
-            if (in_array($field, $this->fields)) {
-                $this->owner->$field = $value;
+            foreach (ArrayHelper::map($query->all(), 'field', 'value') as $field => $value) {
+                if (in_array($field, $this->fields)) {
+                    $this->owner->$field = $value;
+                }
             }
         }
     }
 
-    public function afterSave($event)
-    {
-        /* Save fields values in separate table */
-        $this->deleteModelValues();
-
-        $data = [];
-
-        foreach ($this->fields as $field) {
-            $data[] = [
-                $this->getModelClass(),
-                $this->getPrimaryKey(),
-                $field,
-                $this->owner->$field,
-            ];
-        }
-
-        Yii::$app->db->createCommand()->batchInsert($this->tableName, ['model', 'model_id', 'field', 'value'], $data)->execute();
-    }
-
-    public function deleteModelValues()
+    public function deleteDynamicFieldsValues()
     {
         Yii::$app->db->createCommand()->delete($this->tableName, ['model' => $this->getModelClass(), 'model_id' => $this->getPrimaryKey()])->execute();
     }
 
     protected function getModelClass()
     {
-        return $this->modelName ?: (new \ReflectionClass($this->owner))->getShortName();
+        return $this->modelName ?: StringHelper::basename(get_class($this->owner));
     }
 
     protected function getPrimaryKey()
